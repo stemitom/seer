6cd08c387f5c7a993e4b88d941e3eee707c8883b
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-05 00:05:52 +0100

    Updated Article model

--- api/models.py
+++ api/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-
+    def __str__(self):
+        return self.title
+    
 
 class Comments(models.Model):
     pass
-
-
