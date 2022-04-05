9e615961033256a8fd80f9044ecc949fcef7fcd2
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-05 15:36:51 +0100

    Create necessary apps: core, accounts, articles

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'core',
+    'accounts',
+    'articles'
 ]
 
 MIDDLEWARE = [
