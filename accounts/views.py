59f93c743c535d4048d194d7c05475b33f087784
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-06 12:55:22 +0100

    Removed redundant imports

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,7 @@
 from django.contrib.auth import get_user_model
-
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.generics import (
     CreateAPIView,
-    DestroyAPIView,
     ListAPIView,
-    ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
 )
 from rest_framework.permissions import (
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("accounts.urls")),
-    path("post/", include("articles.urls"))
+    path("article/", include("articles.urls"))
 ]
